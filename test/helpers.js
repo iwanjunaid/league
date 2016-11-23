c0ec98b15a0699a09346f6653b6503db9dc29320
Marc Bachmann <marc.brookman@gmail.com>
2016-11-23 02:08:47 +0100

    chore(package): update uuid to version 3.0.0

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,6 @@
 var Scraper = require('../libs/scraper');
 var assert = require('assert');
-var uuid = require('node-uuid');
+var uuid = require('uuid');
 
 global.assert = assert;
 global.Scraper = Scraper;
